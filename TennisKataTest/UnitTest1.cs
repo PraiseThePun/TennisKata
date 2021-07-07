cf14501efc684150be4222f86764a011a9b9a7bb
Carles Gimenez <gimixpetit@gmail.com>
2021-07-07 15:45:14 +0200

    chore: created project

---
+++ TennisKataTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TennisKataTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
